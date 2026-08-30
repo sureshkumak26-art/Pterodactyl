a98da9c0cfcb6895e25bccf6085c9b5628a8390a
sureshkumak26-art <sureshkumak26@gmail.com>
2026-08-30 10:35:24 +0530

    fix slash command required option ordering

--- src/commands.js
+++ src/commands.js
@@ -15,9 +15,8 @@ module.exports = [
   new SlashCommandBuilder().setName('user').setDescription('View a user').addIntegerOption(o=>o.setName('id').setDescription('User ID').setRequired(true)),
   new SlashCommandBuilder().setName('users').setDescription('List users'),
 
+  // Discord requires every required option to appear before optional options.
   new SlashCommandBuilder().setName('create-server').setDescription('Create a Pterodactyl server')
-    .addIntegerOption(o=>o.setName('user').setDescription('Pterodactyl user ID (use this OR member)').setRequired(false))
-    .addUserOption(o=>o.setName('member').setDescription('Discord member whose Pterodactyl account should be used').setRequired(false))
     .addIntegerOption(o=>o.setName('node').setDescription('Node ID').setRequired(true))
     .addIntegerOption(o=>o.setName('nest').setDescription('Nest ID').setRequired(true))
     .addIntegerOption(o=>o.setName('egg').setDescription('Egg ID').setRequired(true))
@@ -26,9 +25,14 @@ module.exports = [
     .addIntegerOption(o=>o.setName('memory').setDescription('RAM in MB').setRequired(true))
     .addIntegerOption(o=>o.setName('disk').setDescription('Disk in MB').setRequired(true))
     .addIntegerOption(o=>o.setName('cpu').setDescription('CPU percentage').setRequired(true))
+    .addIntegerOption(o=>o.setName('user').setDescription('Pterodactyl user ID (use this OR member)').setRequired(false))
+    .addUserOption(o=>o.setName('member').setDescription('Discord member whose Pterodactyl account should be used').setRequired(false))
     .addIntegerOption(o=>o.setName('backups').setDescription('Backup limit').setRequired(false)),
 
-  new SlashCommandBuilder().setName('delete-server').setDescription('Delete a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)).addBooleanOption(o=>o.setName('force').setDescription('Force delete')),
+  new SlashCommandBuilder().setName('delete-server').setDescription('Delete a server')
+    .addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true))
+    .addBooleanOption(o=>o.setName('force').setDescription('Force delete').setRequired(false)),
+
   new SlashCommandBuilder().setName('server').setDescription('View a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
   new SlashCommandBuilder().setName('servers').setDescription('List servers'),
   new SlashCommandBuilder().setName('rename-server').setDescription('Rename a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)).addStringOption(o=>o.setName('name').setDescription('New server name').setRequired(true)),
