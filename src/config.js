9e0a74b9606641d31f1c0bc15519f1ad147777e3
sureshkumak26-art <sureshkumak26@gmail.com>
2026-08-30 10:28:05 +0530

    Fix Pterodactyl URL normalization

--- src/config.js
+++ src/config.js
@@ -6,11 +6,16 @@ const required = (name) => {
   return value;
 };
 
+// PTERO_URL should normally be the panel root. If /api/application was
+// accidentally included, remove it so the API path is not duplicated.
+const rawPteroUrl = required('PTERO_URL').replace(/\/+$/, '');
+const pteroUrl = rawPteroUrl.replace(/\/api\/application\/?$/i, '');
+
 module.exports = {
   discordToken: required('DISCORD_TOKEN'),
   clientId: required('CLIENT_ID'),
   guildId: required('GUILD_ID'),
-  pteroUrl: required('PTERO_URL').replace(/\/+$/, ''),
+  pteroUrl,
   pteroApiKey: required('PTERO_API_KEY'),
   adminRoleId: required('ADMIN_ROLE_ID'),
   emailDomain: process.env.PTERO_EMAIL_DOMAIN || 'example.com'
