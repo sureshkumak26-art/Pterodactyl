56d4b55c8797b43bfd81355f48566cd1b78530f9
sureshkumak26-art <sureshkumak26@gmail.com>
2026-08-30 10:18:34 +0530

    Add help and server power commands

--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = [
+  new SlashCommandBuilder().setName('help').setDescription('Show all Pterodactyl bot commands'),
   new SlashCommandBuilder().setName('createuser').setDescription('Create a Pterodactyl user')
     .addStringOption(o=>o.setName('username').setDescription('Username').setRequired(true))
-    .addStringOption(o=>o.setName('email').setDescription('Email').setRequired(true))
+    .addStringOption(o=>o.setName('email').setDescription('Email address').setRequired(true))
     .addStringOption(o=>o.setName('firstname').setDescription('First name').setRequired(true))
     .addStringOption(o=>o.setName('lastname').setDescription('Last name').setRequired(true))
     .addStringOption(o=>o.setName('password').setDescription('Password').setRequired(true)),
@@ -11,21 +12,25 @@ module.exports = [
   new SlashCommandBuilder().setName('user').setDescription('View a user').addIntegerOption(o=>o.setName('id').setDescription('User ID').setRequired(true)),
   new SlashCommandBuilder().setName('users').setDescription('List users'),
   new SlashCommandBuilder().setName('create-server').setDescription('Create a server')
-    .addIntegerOption(o=>o.setName('user').setDescription('User ID').setRequired(true))
+    .addIntegerOption(o=>o.setName('user').setDescription('Pterodactyl user ID').setRequired(true))
     .addIntegerOption(o=>o.setName('node').setDescription('Node ID').setRequired(true))
     .addIntegerOption(o=>o.setName('nest').setDescription('Nest ID').setRequired(true))
     .addIntegerOption(o=>o.setName('egg').setDescription('Egg ID').setRequired(true))
     .addIntegerOption(o=>o.setName('allocation').setDescription('Allocation ID').setRequired(true))
     .addStringOption(o=>o.setName('name').setDescription('Server name').setRequired(true))
-    .addIntegerOption(o=>o.setName('memory').setDescription('RAM MB').setRequired(true))
-    .addIntegerOption(o=>o.setName('disk').setDescription('Disk MB').setRequired(true))
-    .addIntegerOption(o=>o.setName('cpu').setDescription('CPU %').setRequired(true))
+    .addIntegerOption(o=>o.setName('memory').setDescription('RAM in MB').setRequired(true))
+    .addIntegerOption(o=>o.setName('disk').setDescription('Disk in MB').setRequired(true))
+    .addIntegerOption(o=>o.setName('cpu').setDescription('CPU percentage').setRequired(true))
     .addIntegerOption(o=>o.setName('backups').setDescription('Backup limit').setRequired(false)),
   new SlashCommandBuilder().setName('delete-server').setDescription('Delete a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)).addBooleanOption(o=>o.setName('force').setDescription('Force delete')),
   new SlashCommandBuilder().setName('server').setDescription('View a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
   new SlashCommandBuilder().setName('servers').setDescription('List servers'),
+  new SlashCommandBuilder().setName('rename-server').setDescription('Rename a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)).addStringOption(o=>o.setName('name').setDescription('New server name').setRequired(true)),
   new SlashCommandBuilder().setName('suspend').setDescription('Suspend a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
   new SlashCommandBuilder().setName('unsuspend').setDescription('Unsuspend a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
+  new SlashCommandBuilder().setName('start').setDescription('Start a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
+  new SlashCommandBuilder().setName('stop').setDescription('Stop a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
+  new SlashCommandBuilder().setName('restart').setDescription('Restart a server').addIntegerOption(o=>o.setName('id').setDescription('Server ID').setRequired(true)),
   new SlashCommandBuilder().setName('nodes').setDescription('List nodes'),
   new SlashCommandBuilder().setName('locations').setDescription('List locations'),
   new SlashCommandBuilder().setName('nests').setDescription('List nests'),
